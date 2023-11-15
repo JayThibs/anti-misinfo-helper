4ef04c5936ecc1f662f9a384b9f06416df631d87
Jacques Thibodeau <thibo.jacques@gmail.com>
2023-11-15 02:57:52 +0000

    Added scoring and threshold for misinfo.

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ sensitive_words = load_keywords("data/keywords.csv")
 contentious_issues = load_keywords("data/contentious_issues.csv")
 
 # Example usage
-tweet = "Example tweet content."
+tweets = ["I hate the new update!", "I love the new update!", "QAnon is a true."]
 likes = 150
+misinfo_estimate_threshold = 50
 
-analysis_result = analyze_tweet(tweet, likes, 100, sensitive_words)
-print("Analysis Result:", analysis_result)
+for tweet in tweets:
+    analysis_result, score = analyze_tweet(tweet, likes, 100, sensitive_words)
+    print("Analysis Result:", analysis_result)
 
-if "needs review" in analysis_result:
+# Decide further actions based on the score
+if score > misinfo_estimate_threshold:
+    # If the score is above a certain threshold, proceed with further actions
     use_gpt4_for_assistance = likes >= 10000
     note_assistance = assist_with_community_note(tweet, use_gpt4_for_assistance)
     print("Assisted Community Note:", note_assistance)
